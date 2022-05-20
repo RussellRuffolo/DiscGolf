694fbf8519ce9fa2416926b104e2e769b5bf3e52
Nolan Boyle <nolanmb21@gmail.com>
2022-05-19 21:14:21 -0400

    Replay Camera

---
+++ QuestTest/Assets/ActionReplayRecord.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionReplayRecord
+{
+    public Vector3 position;
+
+    public Quaternion rotation;
+}
+
